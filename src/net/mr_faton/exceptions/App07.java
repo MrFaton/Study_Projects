c7a36f4a9abc45e1e7cc332ad1b72c9d1055168f
MrFaton <firefly90@inbox.ru>
2014-08-30 15:56:16 +0300

    update from work

--- src/net/mr_faton/exceptions/App07.java
+++ src/net/mr_faton/exceptions/App07.java
@@ -4,4 +4,16 @@ package net.mr_faton.exceptions;
  * Created by root on 30.08.2014.
  */
 public class App07 {
+    public static void main(String[] args) {
+        System.err.println("0");
+        try {
+            System.err.println("1");
+            if (true) throw new Error();
+        } finally {
+            System.err.println("2");
+            if (true)
+                throw new RuntimeException(); //вылетаем по RuntimeException, хотя изначально должны были вылететь по Error
+        }
+        System.err.println("3");
+    }
 }
