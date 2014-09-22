48d4c9c13f94624ca487e54cc2e44dbd43f8fb40
MrFaton <firefly90@inbox.ru>
2014-09-22 19:42:06 +0300

    update from home

--- src/net/mr_faton/Different_Things/PublicTest.java
+++ src/net/mr_faton/Different_Things/PublicTest.java
@@ -1,12 +1,24 @@
 package net.mr_faton.Different_Things;
 
 
+import java.io.*;
+import java.net.URL;
+
 /**
  * Created by Faton on 16.09.2014.
  */
 public class PublicTest {
-    public static void main(String[] args) {
-        System.out.println("dfs");
-//        System.setOut();
+    public static void main(String[] args) throws IOException {
+        InputStream src = new URL("http://expogas.net/").openStream();
+        OutputStream dst = new FileOutputStream("c:/tempIO.txt");
+        copy(src, dst);
+    }
+
+    public static void copy(InputStream src, OutputStream dst) throws IOException {
+        int data;
+        for (int k = 0; k < 3; k++) {
+            data = src.read();
+            dst.write(data);
+        }
     }
 }
---
+++ src/net/mr_faton/IO/App03.java
@@ -0,0 +1,29 @@
+package net.mr_faton.IO;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * Created by Faton on 22.09.2014.
+ */
+public class App03 {
+    public static void main(String[] args) throws IOException {
+        InputStream src = new URL("http://expogas.net/").openStream();
+        OutputStream dst = new FileOutputStream("c:/tempIO.txt");
+        copy(src, dst);
+    }
+
+    public static void copy(InputStream src, OutputStream dst) throws IOException {
+        while (true) {
+            int data = src.read();
+            if (data != -1) {
+                dst.write(data);
+            } else {
+                return;
+            }
+        }
+    }
+}
