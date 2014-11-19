86c6a8231d356a66114421a394f8ab1137272a59
MrFaton <firefly90@inbox.ru>
2014-11-19 11:09:11 +0200

    update from home

---
+++ src/net/mr_faton/OOP/App07_Super/Parent.java
@@ -0,0 +1,27 @@
+package net.mr_faton.OOP.App07_Super;
+
+/**
+ * Created by Faton on 19.11.2014.
+ */
+class Parent {
+    void f() {
+        System.out.println("Это метод родителя");
+    }
+}
+
+class Child extends Parent {
+    void f() {
+        super.f();//Единстенный способ вызвать метод пердка
+        System.out.println("Это метод потомка");
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Parent ref = new Child();
+        ref.f();
+    }
+}
+/*Единственный вариант использования "super.". Это всё используется для вызова метода именно предка,
+а не переопределённого метода потомка.
+ */
