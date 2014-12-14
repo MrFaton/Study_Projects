4726f46238d19c76d1a9c79e7f7ba09463dec8b2
Mr_Faton <firefly90@inbox.ru>
2014-12-14 15:33:08 +0200

    update from work

---
+++ src/net/mr_faton/By_Herbert_Schildt/IO/Applet00/SimpleApplet.java
@@ -0,0 +1,14 @@
+package net.mr_faton.By_Herbert_Schildt.IO.Applet00;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by root on 14.12.2014.
+ */
+
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Привет - это апплет", 20, 20);
+    }
+}
