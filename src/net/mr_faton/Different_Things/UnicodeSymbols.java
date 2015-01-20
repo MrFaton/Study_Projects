bd616fe9fc44090bd877309ae4366ecf45c92879
Mr_Faton <firefly90@inbox.ru>
2015-01-20 21:22:44 +0200

    update from work

---
+++ src/net/mr_faton/Different_Things/UnicodeSymbols.java
@@ -0,0 +1,12 @@
+package net.mr_faton.Different_Things;
+
+/**
+ * Created by root on 20.01.2015.
+ */
+public class UnicodeSymbols {
+    public static void main (String[] args) {
+        System.out.println('\u2746');
+        System.out.println("Привет \u269D");
+        System.out.println("Привет" + '\u269D');
+    }
+}
--- src/net/mr_faton/Test/Test.java
+++ src/net/mr_faton/Test/Test.java
@@ -4,11 +4,6 @@ package net.mr_faton.Test;
 
 public class Test {
     public static void main(String[] args) {
-        Thread th = new Thread(new Thread(){
-            public void run (){
-                System.out.println("Hello");
-            }
-        });
-        th.start();
+        System.out.println('\u2746');
     }
 }
