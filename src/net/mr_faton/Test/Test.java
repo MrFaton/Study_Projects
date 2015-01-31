a6a566694535aed8dbef03b37528100715e0f135
Mr_Faton <firefly90@inbox.ru>
2015-01-31 11:42:37 +0200

    update from work

--- src/net/mr_faton/Test/Test.java
+++ src/net/mr_faton/Test/Test.java
@@ -1,19 +1,24 @@
 package net.mr_faton.Test;
 
-
-import java.text.DateFormatSymbols;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Test {
     public static void main(String[] args) {
-        GregorianCalendar cal = new GregorianCalendar();
-//        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-//        cal.set(Calendar.DAY_OF_MONTH, 1);
-//        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-//        System.out.println(cal.getFirstDayOfWeek());
-        String[] days = new DateFormatSymbols().getShortWeekdays();
-        System.out.println(days[cal.get(Calendar.DAY_OF_WEEK)]);
+        List<Integer> list1 = new LinkedList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        print(list1);
+    }
+
+
+    public static <E> void print (Collection<E> collection) {
+        for (E element : collection) {
+            System.out.println(element);
+        }
     }
 }
