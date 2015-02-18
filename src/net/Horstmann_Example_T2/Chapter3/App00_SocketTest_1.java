ce27a0cb67acb652ae8aed5ba80db40227bf1f04
Mr_Faton <firefly90@inbox.ru>
2015-02-18 19:36:59 +0200

    update from home

---
+++ src/net/Horstmann_Example_T2/Chapter3/App00_SocketTest_1.java
@@ -0,0 +1,25 @@
+package net.Horstmann_Example_T2.Chapter3;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * Created by Mr_Faton on 18.02.2015.
+ */
+public class App00_SocketTest_1 {
+    public static void main(String[] args) {
+        try(Socket socket = new Socket()) {
+//            socket.connect(new InetSocketAddress("time-A.timefreq.bldrdoc.gov", 13), 5000);
+            socket.connect(new InetSocketAddress("horstmann.com", 80), 5000);
+            socket.setSoTimeout(10000);
+            InputStream inputStream = socket.getInputStream();
+            Scanner scanner = new Scanner(inputStream);
+            while (scanner.hasNext()) {
+                System.out.println(scanner.next());
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
