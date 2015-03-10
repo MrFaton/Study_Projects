1e8f46c8c3bad9036c7c22ef31fdf5683f4e48fe
Mr_Faton <firefly90@inbox.ru>
2015-03-10 20:00:21 +0200

    update from home

---
+++ src/net/mr_faton/Different_Things/FolderMaker.java
@@ -0,0 +1,12 @@
+package net.mr_faton.Different_Things;
+
+import java.io.File;
+
+/**
+ * Created by Mr_Faton on 10.03.2015.
+ */
+public class FolderMaker {
+    public static void main(String[] args) {
+        new File("C:/" + "net\\Junior\\Servlets\\WebApp_V1").mkdirs();
+    }
+}
