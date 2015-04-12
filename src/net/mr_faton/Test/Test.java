ff0f2b88f369c7146a5aee11333140b512a3a895
Mr_Faton <firefly90@inbox.ru>
2015-04-12 21:33:11 +0300

    update from home

--- src/net/mr_faton/Test/Test.java
+++ src/net/mr_faton/Test/Test.java
@@ -1,16 +1,8 @@
 package net.mr_faton.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-
+        System.out.println();
     }
 }
 
