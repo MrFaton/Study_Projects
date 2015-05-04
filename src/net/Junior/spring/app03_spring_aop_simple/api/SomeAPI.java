6f7ae41ab87a50f5d930231423aa8b03f1caa5ca
MrFaton <firefly90@inbox.ru>
2015-05-04 23:02:52 +0300

    updated

--- src/net/Junior/spring/app03_spring_aop_simple/api/SomeAPI.java
+++ src/net/Junior/spring/app03_spring_aop_simple/api/SomeAPI.java
@@ -4,6 +4,7 @@ package net.Junior.spring.app03_spring_aop_simple.api;
  * Created by root on 03.05.2015.
  */
 public interface SomeAPI {
+    public
     public void sayHi();
 
     public void saySomething(String someThing);
