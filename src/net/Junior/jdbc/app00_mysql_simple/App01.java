467ccfa0ff325ab4b9a04a6ad8571b3e9faa8c1d
Mr_Faton <firefly90@inbox.ru>
2015-05-11 19:33:08 +0300

    update from work

--- src/net/Junior/jdbc/app00_mysql_simple/App01.java
+++ src/net/Junior/jdbc/app00_mysql_simple/App01.java
@@ -8,7 +8,9 @@ import java.util.Properties;
  */
 public class App01 {
     public static final String JDBC_URL =
-            "jdbc:mysql://127.0.0.1:3306/my_test_db?user=Mr_Faton&password=123";
+            "jdbc:mysql://127.0.0.1:3306/my_test_db?user=Mr_Faton&password=123";//DB at my home
+//    public static final String JDBC_URL =
+//            "jdbc:mysql://192.168.101.1:3306/my_test_db?user=Mr_Faton&password=123";//DB at my work
 
     public static void main(String[] args) throws SQLException {
         java.sql.Driver driver = new com.mysql.jdbc.Driver();
