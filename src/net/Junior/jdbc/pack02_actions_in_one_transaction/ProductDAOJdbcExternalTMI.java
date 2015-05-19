487a895b4d905d60e20d15aa2b0275473c0d2801
Mr_Faton <firefly90@inbox.ru>
2015-05-19 14:08:08 +0300

    update from home

--- src/net/Junior/jdbc/pack02_actions_in_one_transaction/ProductDAOJdbcExternalTMI.java
+++ src/net/Junior/jdbc/pack02_actions_in_one_transaction/ProductDAOJdbcExternalTMI.java
@@ -1,11 +1,12 @@
 package net.Junior.jdbc.pack02_actions_in_one_transaction;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * Created by Mr_Faton on 18.05.2015.
  */
-public class ProductDAOJdbcExternalTMI {
+public class ProductDAOJdbcExternalTMI implements ProductDAO {
     public static final String JDBC_URL =
             "jdbc:mysql://127.0.0.1:3306/my_test_db?user=Mr_Faton&password=123";//DB at my home
 
@@ -16,5 +17,15 @@ public class ProductDAOJdbcExternalTMI {
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    @Override
+    public Product selectByID(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> selectAll() {
+        return null;
     }
 }
