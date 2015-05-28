d799f39be58d1e772f7d145c67483f003e540af5
Mr_Faton <firefly90@inbox.ru>
2015-05-28 19:15:28 +0300

    update from home

--- src/net/mr_faton/Test/Test.java
+++ src/net/mr_faton/Test/Test.java
@@ -1,10 +1,20 @@
 package net.mr_faton.Test;
 
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir") + "\\cache");
+        Set<String> set = new LinkedHashSet<>();
+        set.add("Hi");
+        set.add("hello");
+        set.add("hi");
+        set.add("poom");
+        set.add("hi");
+        set.add("end");
 
+        System.out.println(set);
     }
 
 }
