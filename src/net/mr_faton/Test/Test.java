4472d379c39f35d2d32b723e9cde01a6eb01b980
Mr_Faton <firefly90@inbox.ru>
2015-06-11 13:17:36 +0300

    update from home

--- src/net/mr_faton/Test/Test.java
+++ src/net/mr_faton/Test/Test.java
@@ -1,27 +1,19 @@
 package net.mr_faton.Test;
 
-import java.sql.*;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class Test {
     public static void main(String[] args) {
-        String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/tweagle?user=Mr_Faton&password=123";//DB at home
-        String sql =
-                "SELECT next_tweet, next_mention, next_retweet, next_dm, next_follow, next_followback, next_unfollow, " +
-                        "next_ac_text_status " +
-                        "FROM tweagle.twitter_users;";
+        GregorianCalendar calendar = new GregorianCalendar();
+//        calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 14);
 
-        String SELECT_SQL = "SELECT MIN(LEAST);";
-        try (Connection connection = DriverManager.getConnection(JDBC_URL)) {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                long result = resultSet.getLong(1);
-                System.out.println(result);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+        System.out.println(String.format("Calendar: %td : %<tm : %<tY | %<tH : %<tM", calendar));
     }
 }
