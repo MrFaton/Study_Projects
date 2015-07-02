f0ce5b34c0850d6f23a7901b796e896f3d9250a9
Mr_Faton <firefly90@inbox.ru>
2015-07-02 16:31:25 +0300

    update from work

--- src/net/mr_faton/Test/Test2.java
+++ src/net/mr_faton/Test/Test2.java
@@ -1,6 +1,7 @@
 package net.mr_faton.Test;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -8,31 +9,13 @@ import java.util.TreeSet;
  * Created by Mr_Faton on 23.06.2015.
  */
 public class Test2 {
-    private static final File FILE_SER = new File("D:\\Twitter\\UniqueWordCollection.ser");
 
     public static void main(String[] args) {
-        String str = "просто,as";
-        String cleanToken = str.replaceAll("[^а-яА-Яa-zA-Z]", "");
-        System.out.println(cleanToken);
-
-        Set<String> set = new TreeSet<>();
-        set.add("привет");
-        set.add("азбука");
-        set.add("якорь");
-        set.add("итер");
-        set.add("с");
-        set.add("aspect");
-        set.add("итер");
-        set.add("с");
-        System.out.println(set);
-
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(FILE_SER))) {
-            objectOutputStream.writeObject(set);
-            objectOutputStream.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String str = "123.456";
+        int pointPos = str.indexOf(".");
+        int num1 = Integer.valueOf(str.substring(0, pointPos));
+        int num2 = Integer.valueOf(str.substring(pointPos + 1, str.length()));
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
     }
 }
